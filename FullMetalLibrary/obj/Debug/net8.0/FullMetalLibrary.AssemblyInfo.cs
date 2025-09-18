24006a5912fca5df5c4b4a596205a2aa011be4d9
Dave Sushames <david.sushames@gmail.com>
2025-09-18 12:53:50 +1200

    added stylesheet and made it pretty

--- FullMetalLibrary/obj/Debug/net8.0/FullMetalLibrary.AssemblyInfo.cs
+++ FullMetalLibrary/obj/Debug/net8.0/FullMetalLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullMetalLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178111bd020553342d93da8116896fe84c5b9489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea70c572e5e2805b2873cf2a6e4c55c229925e88")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullMetalLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullMetalLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
