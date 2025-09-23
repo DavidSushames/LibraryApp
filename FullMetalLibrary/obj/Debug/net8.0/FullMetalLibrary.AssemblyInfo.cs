4c48dec36026e4efd5fbc93de5ee874eda0980ca
sachith999 <sachith.samarasinghe4@gmail.com>
2025-09-23 15:12:12 +1200

    Removing Search bar

--- FullMetalLibrary/obj/Debug/net8.0/FullMetalLibrary.AssemblyInfo.cs
+++ FullMetalLibrary/obj/Debug/net8.0/FullMetalLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullMetalLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d559b34afe1c9c60c6d970a3acab78b25b5e06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d50881bdf48cd14676f128ffeac227df88666e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullMetalLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullMetalLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
