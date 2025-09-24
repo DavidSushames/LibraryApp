822b6e787509a39c6be17d47fc039fa4a587a00c
sachith999 <sachith.samarasinghe4@gmail.com>
2025-09-24 20:58:30 +1200

    Default admin, Login validation fix

--- FullMetalLibrary/Models/LoginViewModel.cs
+++ FullMetalLibrary/Models/LoginViewModel.cs
@@ -5,14 +5,14 @@ namespace FullMetalLibrary.Models
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Please enter a valid email address with '@'.")]
+        [EmailAddress(ErrorMessage = "Invalid Email or Password")]
         [Display(Name = "Email")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-            ErrorMessage = "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character, Don't be dumb to use week password.")]
+            ErrorMessage = "Invalid Email or Password")]
         public string Password { get; set; } = string.Empty;
     }
 }
